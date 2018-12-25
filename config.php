69973095ae7a9c82fde5871023353d0c1816cb56
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2018-12-25 16:45:17 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,15 @@
-
+<?php
+	$hostname = "127.0.0.1";
+	$username = "abu";
+	$pass = "password";
+	$dataBaseName = "restaurant"; 
+	$connection = mysqli_connect($hostname, $username, $pass);
+	$selection = mysqli_select_db($connection, $dataBaseName);
+	$success = true;
+	if(!$connection){
+		echo "combe";
+	}
+if(!$selection){
+	echo "abu";
+}
+?>
